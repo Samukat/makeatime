f387eb7466c5f8d279efcd2b9b33fad5c3cc01aa
Samuel Katz <samuelkatz2245@gmail.com>
2023-04-22 17:13:05 +1000

    random_testing

--- makeatime/src/App.tsx
+++ makeatime/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DateTimePicker from 'react-datetime-picker';
+
+import React, { useState } from 'react';
+import TimeSheet from './TimeSheet';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+  const [value, onChange] = useState(new Date());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TimeSheet />
+      <textarea>
+        add a description / title for your event
+      </textarea>
+      <div>
+        <DateTimePicker value={value} />
+      </div>
+    </>
+  )
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to cool.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
