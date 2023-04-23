4225a3e432521072cee30994605ee00efd9f2f8e
peter-osborne <peterosborne248@gmail.com>
2023-04-23 19:54:50 +1000

    clean slate

--- makeatime/src/App.tsx
+++ makeatime/src/App.tsx
@@ -1,9 +1,4 @@
-import DateTimePicker from 'react-datetime-picker';
-
 import React, { useState } from 'react';
-import TimeSheet from './TimeSheet';
-// import logo from './logo.svg';
-// import './App.css';
 
 function App() {
   const [value, onChange] = useState(new Date());
