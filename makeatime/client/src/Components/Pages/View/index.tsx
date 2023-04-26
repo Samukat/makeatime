c274b4a2bd7af17e6e7a5ee4046d0caff513b0a7
peter-osborne <peterosborne248@gmail.com>
2023-04-26 22:14:00 +1000

    backend get and pull

--- makeatime/client/src/Components/Pages/View/index.tsx
+++ makeatime/client/src/Components/Pages/View/index.tsx
@@ -6,15 +6,32 @@ interface BackendData {
 }
 
 const View: React.FC = () => {
-  const [backendData, setBackendData] = useState<BackendData>({});
 
+
+  const [backendData, setBackendData] = useState<BackendData>({});
   useEffect(() => {
-    fetch("/api")
+    fetch("/get_name")
       .then(response => response.json())
       .then(data => {
         setBackendData(data);
       });
   }, []);
+
+
+
+
+  const [name, setName] = useState("")
+  const [postID, setPostId] = useState("");
+  useEffect(() => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: {name} })
+    };
+    fetch('http://localhost:4000/post_name', requestOptions)
+        .then(response => response.json())
+        .then(data => setPostId(data.id));
+  }, [name]);
 
   return (
     <>
@@ -24,6 +41,12 @@ const View: React.FC = () => {
         ) : (
           backendData.users.map((user, i) => <p key={i}>{user}</p>)
         )}
+      </div>
+      <div className='api-response'>
+        Look at Console, we are posting!
+      </div>
+      <div>
+          <input type="text" onChange={(e) => setName(e.target.value)} />
       </div>
     </>
   );
--- makeatime/server/server.js
+++ makeatime/server/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const port = 4000
+const cors = require('cors')
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+app.get("/get_name", cors(), async (req, res) => {
+    res.send({"users":["Neil", "Buzz", "Crampton"]})
 })
 
-app.listen(4000, () => {console.log("Server started on port 4000")})
+app.post("/post_name", async (req, res) => {
+    let {name} = req.body
+    console.log(name);
+})
+app.listen(port, () => {console.log("Server started on port 4000")})
