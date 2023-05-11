80438a2e0ca6dc4253e134b7467f9f85dc0643af
Samuel Katz <samuelkatz2245@gmail.com>
2023-05-11 15:46:25 +1000

    nav done

--- client/src/components/layout/header.nav.tsx
+++ client/src/components/layout/header.nav.tsx
@@ -4,32 +4,53 @@ import {NavLink} from "react-router-dom";
 // import Navbar from 'react-bootstrap/Navbar';
 // import Container from 'react-bootstrap/Container';
 import './style.scss';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import {Container, Navbar, Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const NavigationBar  = () => {
     return (
         <div className="nav-style">
-            <Navbar >
+            <Navbar className="nav-style-bar" variant="dark"> {/* //bg="dark"  */}
+                <Container className='content-width-bar'>
+                
+                
+                    <NavLink className='navbar-brand' to='/'>
+                        <img  alt="logo" className="logo-image"/>
+                        MakeATime</NavLink>
+                    
+                    <Nav className="me-auto">
+                        <NavLink to="/create" className='nav-link'> Create Event</NavLink>
+                        <NavLink to="/view" className='nav-link'> View Event</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <Container className='nav-sub'>
+                MakeATime
+            </Container>
+        </div>
+
+        
+        // <div className="nav-style">
+        //     <Navbar >
                 
 
-                <header className='nav-bar'>
-                    <Navbar.Brand className='nav-logo' href="\">
-                        <img alt="logo" className="logo-image"/>
-                            MakeATime
-                    </Navbar.Brand>
+        //         <header className='nav-bar'>
+        //             <Navbar.Brand className='nav-logo' href="\">
+        //                 <img alt="logo" className="logo-image"/>
+        //                     MakeATime
+        //             </Navbar.Brand>
 
-                    <nav className="nav-links">
-                        <NavLink to="/create">Create Events</NavLink>
-                        <NavLink to="/event">View Event</NavLink>
-                    </nav>
-                </header>
-            </Navbar>
+        //             <nav className="nav-links">
+        //                 <NavLink to="/create">Create Events</NavLink>
+        //                 <NavLink to="/event">View Event</NavLink>
+        //             </nav>
+        //         </header>
+        //     </Navbar>
 
-            <Container className='nav-sub'>
-                MakeATime sub 
-            </Container>
+        //     <Container className='nav-sub'>
+        //         MakeATime sub 
+        //     </Container>
 
-        </div>
+        // </div>
     ) 
 }
