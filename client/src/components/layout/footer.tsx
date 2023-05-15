abfd42b0a87a3a0f2df4dd1ffc249d21cf65452b
peter-osborne <peterosborne248@gmail.com>
2023-05-15 12:06:59 +1000

    Created about page link in footer

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {Container} from 'react-bootstrap'
+import {NavLink} from "react-router-dom";
 
 // import Navbar from 'react-bootstrap/Navbar';
 // import Container from 'react-bootstrap/Container';
@@ -12,12 +13,13 @@ export const Footer = () => {
                 <div className='footer-component'>
                     © makeatime 2023
                 </div>
-                <div className='footer-component'>
-                    Privacy
-                </div>
+                <NavLink className='footer-link' to="./about">
+                    About
+                </NavLink>
                 <div className='footer-component last-comp'>
                     2023
                 </div>
+                
             </Container>
         </footer>
     ) 
