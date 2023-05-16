3e0082db1d46b7f3d1216302809da9009076be1e
Samuel Katz <samuelkatz2245@gmail.com>
2023-05-16 20:57:42 +1000

    a

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
-const PORT = 3001;
+const PORT = 3000;
 const app = express();
 
 
-const cors=require("cors");
+const cors = require("cors");
 const corsOptions ={
-   origin:'*', 
+   origin:'*',    //maybe change...? depends how open we want this.. (note, hosting ip is automaticcaly allowed --> no need to leave open or configure if client and server on same ip)
+   method: "GET, POST", //to do: PUT? https://stackoverflow.com/questions/630453/what-is-the-difference-between-post-and-put-in-http
    credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
+   optionSuccessStatus:200 //really only for legacy browser
 }
-
-
 app.use(cors(corsOptions)) // Use this after the variable declaration
+
+app.set('trust proxy', true); //for ip stuff (id)
+
+
 //routes 
 const testRoutes = require("./routes/test");
 const testData = require("./routes/testing_data");
 
-
-
-app.set('trust proxy', true); //for ip stuff 
 
 //routes 
 app.use('/test', testRoutes);
