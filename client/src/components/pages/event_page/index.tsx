1bee38a019189b9d4574a25152e5d63741da39e5
peter-osborne <peterosborne248@gmail.com>
2023-05-25 13:27:38 +1000

    Button for weekly/ daily view (no functionality)

--- client/src/components/pages/event_page/index.tsx
+++ client/src/components/pages/event_page/index.tsx
@@ -3,9 +3,11 @@ import './index.scss';
 import { LoaderFunctionArgs, useLoaderData, useParams } from 'react-router-dom';
 import DaySelector from './daySelectorView';
 import Time from '../../../helpers/Time';
+import classNames from 'classnames'
 
 export default function Event() {
 
+    const [viewFormat, setViewFormat] = useState('weekly');
     const { id } = useParams();
 
     const eventdata = useLoaderData();
@@ -19,6 +21,10 @@ export default function Event() {
     return (
         <div className='event'>
             <h1 className='event-title'> Event: {data.eventName}</h1>
+            <div className='view-format-selector'>
+                <button className={classNames('weekly-selector', `weekly-selector-${(viewFormat === 'weekly') ? 'selected' : 'unselected'}`)} onClick={() => { setViewFormat('weekly') }}>Weekly View</button>
+                <button className={classNames('daily-selector', `daily-selector-${(viewFormat === 'daily') ? 'selected' : 'unselected'}`)} onClick={() => { setViewFormat('daily') }}>View All Events</button>
+            </div>
 
             <DaySelector
                 calendarType={data.calendarType}
